c44b5247c62960b098e85a9315063ecd87c3dad7
Aya Mydani <ayamidani98@gmail.com>
2025-09-08 18:23:32 +0300

    feat: complete route setup for app structure

--- client/src/components/admin-view/layout.jsx
+++ client/src/components/admin-view/layout.jsx
@@ -1,9 +1,13 @@
+import AdminHeader from "./header";
+import AdminSideBar from "./sidebar";
+import { Outlet } from "react-router-dom";
+
 function AdminLayout(){
     return(
         <div className="flex min-h-screen w-full">
-            {/* admin sidebar */}
+            <AdminSideBar />
             <div className="flex flex-1 flex-col">
-                {/* header sidebar */}
+                <AdminHeader />
                 <main className="flex-1 flex bg-muted/40 p-4 md:p-6">
                     <Outlet/>
                 </main>
