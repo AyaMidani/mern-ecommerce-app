96db9252836a5f8b606982eb95d59ec025dee1f8
Aya Mydani <ayamidani98@gmail.com>
2025-09-08 19:29:55 +0300

    feat: implement role-based routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import AdminLayout from "./components/admin-view/layout";
+import AuthLayout from "./components/auth/layout"
+import AdminDashboard from "./pages/auth/admin-view/dashboard";
+import AdminOrders from "./pages/auth/admin-view/orders";
+import AuthLogin from "./pages/auth/login"
+import AuthRegister from "./pages/auth/register"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AdminFeatures from "./pages/auth/admin-view/features";
+import AdminProdcuts from "./pages/auth/admin-view/products";
+import ShoppingLayout from "./components/shopping-view/layout";
+import NotFound from "./pages/not-found";
+import ShoppingHome from "./pages/auth/shopping-view/home";
+import ShoppingListing from "./pages/auth/shopping-view/listing";
+import ShoppingCheckout from "./pages/auth/shopping-view/checkout";
+import CheckAuth from "./components/common/check-auth";
+import UnauthPage from "./pages/unauth-page";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const isAuthenticated=false;
+  const user=null;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="flex flex-col overflow-hidden bg-white">
+      <Routes>
+        <Route path="/auth" element={
+          <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+            <AuthLayout />
+          </CheckAuth>
+        }>
+          <Route path="login" element={<AuthLogin />}/>
+          <Route path="register" element={<AuthRegister />}/>
+        </Route>
+
+          <Route path="/admin" element={
+          <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+            <AdminLayout />
+          </CheckAuth>
+            }>
+          <Route path="dashboard" element={<AdminDashboard />}/>
+          <Route path="orders" element={<AdminOrders />}/>
+          <Route path="products" element={<AdminProdcuts />}/>
+          <Route path="features" element={<AdminFeatures />}/>
+        </Route>
+
+        <Route path="/shop" element={
+          <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+            <ShoppingLayout />
+          </CheckAuth>
+          }>
+          <Route path="home" element={<ShoppingHome />}/>
+          <Route path="listing" element={<ShoppingListing />}/>
+          <Route path="checkout" element={<ShoppingCheckout />}/>
+          <Route path="account" element={<ShoppingCheckout />}/>
+        </Route>
+        <Route path="/unauth-page" element={< UnauthPage/>}></Route>
+        <Route path="/*" element={< NotFound/>}></Route>
+      </Routes>
+    </div>
   )
 }
 
-export default App
+export default App;
