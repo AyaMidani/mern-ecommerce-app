5639f8e4384374815fdd1e2b848ccd2c7b11a47d
Aya Mydani <ayamidani98@gmail.com>
2025-09-20 23:11:29 +0300

    feat(router): implement Address routes in frontend

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -3,13 +3,15 @@ import authReducer from './auth-slice/index.js';
 import AdminProductsSlice from './admin/products-slice';
 import shopProductsSlice from './shop/products-slice'
 import shoppingCartSlice from './shop/cart-slice'
+import shopAddressSlice from './shop/addrss-slice'
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     adminProducts: AdminProductsSlice, 
     shopProducts: shopProductsSlice,
-    shopCart: shoppingCartSlice
+    shopCart: shoppingCartSlice,
+    shopAddress: shopAddressSlice
   },
 });
 
