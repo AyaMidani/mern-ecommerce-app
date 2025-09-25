529b3873749559e60d3adbdd2399b0e662b0eed7
Aya Mydani <ayamidani98@gmail.com>
2025-09-25 19:27:02 +0300

    Routes Fix

--- client/src/store/shop/review-slice/index.js
+++ client/src/store/shop/review-slice/index.js
@@ -1,49 +1,49 @@
-import { createSlice,createAsyncThunk} from '@reduxjs/toolkit';
-import axios from 'axios';
+  import { createSlice,createAsyncThunk} from '@reduxjs/toolkit';
+  import axios from 'axios';
 
 
-const initialState = {
-  isLoading: false,
-  reviews: [],
-};
+  const initialState = {
+    isLoading: false,
+    reviews: [],
+  };
 
-export const addReview= createAsyncThunk('product/addReview',
-  async(data)=>{
-    const response= await axios.post(`http://localhost:5001/api/shop/review/add`,{data});
-    return response?.data;
-  }
-)
-
-export const getReviews= createAsyncThunk('product/getReviews',
-  async(id)=>{ 
-    const response= await axios.get(`http://localhost:5001/api/shop/review/${id}`);
-    return response?.data;
-  }
-)
-
-const reviewSlice = createSlice({
-    name: 'reviewSlice',
-    initialState,
-    reducers: {},
-    extraReducers: (builder) =>{
-        builder.addCase(addReview.pending,(state)=>{
-            state.isLoading=true;
-            }).addCase(addReview.fulfilled,(state,action)=>{
-                state.isLoading=false;
-                state.reviews=action.payload.data;
-            }).addCase(addReview.rejected,(state)=>{
-                state.isLoading=false;
-                state.reviews=[];
-            }).addCase(getReviews.pending,(state)=>{
-            state.isLoading=true;
-            }).addCase(getReviews.fulfilled,(state,action)=>{
-                state.isLoading=false;
-                state.reviews=action.payload.data;
-            }).addCase(getReviews.rejected,(state)=>{
-                state.isLoading=false;
-                state.reviews=[];
-            })
+  export const addReview= createAsyncThunk('product/addReview',
+    async(data)=>{
+      const response= await axios.post(`http://localhost:5001/api/shop/review/add`,{data});
+      return response?.data;
     }
-})
+  )
 
-export default reviewSlice.reducer;
+  export const getReviews= createAsyncThunk('product/getReviews',
+    async(id)=>{ 
+      const response= await axios.get(`http://localhost:5001/api/shop/review/${id}`);
+      return response?.data;
+    }
+  )
+
+  const reviewSlice = createSlice({
+      name: 'reviewSlice',
+      initialState,
+      reducers: {},
+      extraReducers: (builder) =>{
+          builder.addCase(addReview.pending,(state)=>{
+              state.isLoading=true;
+              }).addCase(addReview.fulfilled,(state,action)=>{
+                  state.isLoading=false;
+                  state.reviews=action.payload.data;
+              }).addCase(addReview.rejected,(state)=>{
+                  state.isLoading=false;
+                  state.reviews=[];
+              }).addCase(getReviews.pending,(state)=>{
+              state.isLoading=true;
+              }).addCase(getReviews.fulfilled,(state,action)=>{
+                  state.isLoading=false;
+                  state.reviews=action.payload.data;
+              }).addCase(getReviews.rejected,(state)=>{
+                  state.isLoading=false;
+                  state.reviews=[];
+              })
+      }
+  })
+
+  export default reviewSlice.reducer;
